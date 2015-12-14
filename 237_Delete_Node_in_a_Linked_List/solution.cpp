b2728f50bc4989eebd5cc8abea2478e743107e6d
fluyy <tulneer@gmail.com>
2015-12-15 00:24:11 +0800

    晚上随便做了几道水题

---
+++ 237_Delete_Node_in_a_Linked_List/solution.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+
+struct ListNode {
+     int val;
+     ListNode *next;
+     ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+public:
+    void deleteNode(ListNode* node) {
+		*node = *(node->next);
+    }
+};
+
